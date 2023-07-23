20a3800fd49208faaa81fc25cdc9bd25b248a1ac
sburd <burdspencer@gmail.com>
2023-07-22 21:01:51 -0400

    repo work

--- ProFootballReferenceDB/Workbench.sql
+++
@@ -1,6 +0,0 @@
-exec ssp_ScheduleDataReport_Dev 'B', 0.1,0.1,0.1
-
---SELECT ROUND(AVG(SeasonProfit/SeasonWager),2), SeasonProfit, SeasonWager, OneUnitThreshold, FiveUnitThreshold, TenUnitThreshold, OneUnitThreshold + FiveUnitThreshold + TenUnitThreshold as TotalThreshold
---FROM SeasonDataAggregateCache 
---GROUP BY OneUnitThreshold, FiveUnitThreshold, TenUnitThreshold,  SeasonProfit, SeasonWager
---ORDER BY AVG(SeasonProfit/SeasonWager) DESC
